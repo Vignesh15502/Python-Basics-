022dcf1943074cc44045a0bd5ad5e1376251f871
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-29 15:30:31 +0530

    Add files via upload

---
+++ 26.For Loop.py
@@ -0,0 +1,55 @@
+# For Loop 
+
+#1
+for i in range(0,21,2):
+	print(i)
+    
+'''
+output
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+'''	
+#2
+for i in range(5):
+	a = int(input("Enter the Number:"))
+	b = int(input("Enter the Number:"))
+	i = a+b
+	print(i)
+    
+'''
+output
+
+Enter the Number:456
+Enter the Number:654
+1110
+Enter the Number:876
+Enter the Number:897
+1773
+Enter the Number:7867
+Enter the Number:4345
+12212
+Enter the Number:232
+Enter the Number:654
+886
+Enter the Number:875
+Enter the Number:890
+1765
+'''
+'''
+A for loop is used for iterating over a 
+sequence (that is either a list, a tuple, 
+a dictionary, a set, or a string). 
+This is less like the for keyword in other 
+programming languages, and works more like an 
+iterator method as found in other 
+object-orientated programming languages.
+'''
