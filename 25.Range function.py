d5302a064da9493bab9ee3cc46a4c86635526d10
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-29 15:30:04 +0530

    Add files via upload

---
+++ 25.Range function.py
@@ -0,0 +1,16 @@
+# Range Function
+
+print(list(range(5)))
+print(list(range(2,3)))
+print(list(range(2,20,2)))
+print(list(range(1,20,2)))
+
+'''
+output
+
+[0, 1, 2, 3, 4]
+[2, 3, 4]
+[2, 4, 6, 8, 10, 12, 14, 16, 18]
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+'''
