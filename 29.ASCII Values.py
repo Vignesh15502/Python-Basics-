d19e500fa08b7c94cc7344f160e2c4c4cc8a71ad
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-08-30 18:27:48 +0530

    Add files via upload

---
+++ 29.ASCII Values.py
@@ -0,0 +1,17 @@
+# ASCII vlaues
+
+'''
+ASCII stands for American Standard 
+Code for Information Interchange. 
+It is a character encoding standard 
+that uses numbers from 0 to 127 to represent 
+English characters. For example, ASCII code 
+for the character A is 65, and 90 is for Z. 
+Similarly, ASCII code 97 is for a, and 122 is for z.
+'''
+
+for i in range(65,71,1):
+	for j in range(65,71,1):
+		print(chr(j),end=" ")
+	print(" ")
+
