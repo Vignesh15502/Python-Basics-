782098681636c4147b55a9d39072d8347fa17220
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-04 16:59:23 +0530

    Add files via upload

---
+++ 40.Nested Dictionary .py
@@ -0,0 +1,14 @@
+# Nested Dictionary
+
+users={
+		"user1" : {"name" : "Ram",
+				   "age"  : 25,
+				   "ismarried" : True
+				   },
+		"user2" : {
+				   "name" : "Raman",
+				   "age"  : 35,
+				   "ismarried" : False
+				   }
+	}
+print(users)
