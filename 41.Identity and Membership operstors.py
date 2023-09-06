f064c4a504e66b3a631aa06c67308c8873c9bc74
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-06 16:44:12 +0530

    Add files via upload

---
+++ 41.Identity and Membership operstors.py
@@ -0,0 +1,24 @@
+#Identity operators i python
+
+'''
+is
+isnot
+'''
+a = [1,2]
+b = [1,2]
+c = a
+print(a is c)
+print(a is b)
+
+#Memebership operators in python
+
+'''
+in
+not in
+'''
+
+a = [10,25,45,88]
+
+print(22 in a)
+print(22 not in a)
+print(25 not in a)
